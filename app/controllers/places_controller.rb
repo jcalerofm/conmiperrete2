a590c695d4669de3728cc32387218ab422add6ea
Jorge Calero <jorge.calero@gmail.com>
2020-05-07 13:02:26 +0200

    started place controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,54 @@
 class PlacesController < ApplicationController
+
+  def index
+    @places = @places.all
+  end
+
+  def show
+    @place = place.find(params[:id])
+  end
+
+  def new
+    @place = Place.new
+  end
+
+  def create
+    @place = Place.new(place_params)
+    @place.user = current_user
+    if @place.save
+      redirect_to places_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @place = Place.find(params[:id])
+  end
+
+  def update
+    @place = place.find(params[:id])
+    if @place.update(place_params)
+      redirect_to dashboard_path
+    else
+      render :edit
+    end
+  end
+
+  # def destroy
+  #   set_place
+  #   @place.destroy
+  #   redirect_to dashboard_path
+  # end
+
+
+  private
+
+  def set_place
+    @place = Place.find(params[:id])
+  end
+
+  def place_params
+    params.require(:place).permit(:name, :description, :photo)
+  end
 end

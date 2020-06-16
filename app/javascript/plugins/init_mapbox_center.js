70aaa3c6d17fb96494dcdd1e803c95abdc3af76a
Miguel <quecontentoestoy@gmail.com>
2020-06-16 14:08:09 +0200

    added transparent navbar home, created show page, added container to login form

--- app/javascript/plugins/init_mapbox_center.js
+++ app/javascript/plugins/init_mapbox_center.js
@@ -16,17 +16,6 @@ const initMapboxCenter = (center) => {
       zoom: 10
     });
 
-    var layerList = document.getElementById('menu');
-    var inputs = layerList.getElementsByTagName('input');
-
-    function switchLayer(layer) {
-    var layerId = layer.target.id;
-    map.setStyle('mapbox://styles/mapbox/' + layerId);
-    }
-
-    for (var i = 0; i < inputs.length; i++) {
-    inputs[i].onclick = switchLayer;
-    }
 
 
     const markers = JSON.parse(mapElement.dataset.markers);

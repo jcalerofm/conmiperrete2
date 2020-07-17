95d11a4c75159b86dce701b11f76f04e44fdacef
Miguel <quecontentoestoy@gmail.com>
2020-07-17 23:12:55 +0200

    added username column in devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :places
   validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
 end
